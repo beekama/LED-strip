97aefa1cfae1fe7bf968f13130480e64ddd4984d
beekama <k.aus.w@gmx.de>
2020-12-05 00:21:05 +0100

    rgb

--- webinterface/test-flask.py
+++ webinterface/test-flask.py
@@ -4,20 +4,30 @@ import requests
 import argparse
 
 app = flask.Flask("LOLOL")
-
+app.debug = True
 
 @app.route("/")
 def mainPage():
     return flask.render_template("main.html", example="halloo") 
 
-@app.route("/API")
+@app.route("/API", methods=['GET', 'POST'])
 def modePage():
-    mode = flask.request.args.get("mode")
+    mode = "mode_1"
+    if (flask.request.method=='POST'):
+        mode = flask.request.form["mode"]
+    else:
+        mode = flask.request.args.get("mode")
     if (mode == "mode_1"):
         requests.post('http://192.168.178.199/', data="Ar\n")
-    else:
+    elif (mode == "mode_2"):
         requests.post('http://192.168.178.199/', data="Bg\n")
+    else:
+        mydata = mode+"\n"
+        app.logger.info('my mydata is %s and mode is %s\n', mydata, mode)
+        requests.post('http://192.168.178.199/', data=mydata)
+    app.logger.info('the mode in gereral is %s\n', mode)
     return (204, "")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')    
