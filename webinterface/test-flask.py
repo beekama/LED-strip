b2e0996b23b2abbfa45fbf2a65a3b28167748e62
beekama <k.aus.w@gmx.de>
2020-12-09 00:16:13 +0100

    colorpicker

--- webinterface/test-flask.py
+++ webinterface/test-flask.py
@@ -3,7 +3,7 @@ import flask
 import requests
 import argparse
 
-app = flask.Flask("LOLOL")
+app = flask.Flask("LED-Interface")
 app.debug = True
 
 @app.route("/")
@@ -26,7 +26,7 @@ def modePage():
         app.logger.info('my mydata is %s and mode is %s\n', mydata, mode)
         requests.post('http://192.168.178.199/', data=mydata)
     app.logger.info('the mode in gereral is %s\n', mode)
-    return (204, "")
+    return ("" ,204)
 
 
 if __name__ == "__main__":
