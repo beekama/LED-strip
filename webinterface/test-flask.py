b20748c9c92b3a45d7d22ae991b4e1744f242e37
beekama <k.aus.w@gmx.de>
2020-12-26 00:40:16 +0100

    extended interface

--- webinterface/test-flask.py
+++ webinterface/test-flask.py
@@ -6,28 +6,21 @@ import argparse
 app = flask.Flask("LED-Interface")
 app.debug = True
 
+#default -> mode0:= AUS
+#message = "m000"
+
 @app.route("/")
 def mainPage():
-    return flask.render_template("main.html", example="halloo") 
+    return flask.render_template("main.html", example="halloo")
 
-@app.route("/API", methods=['GET', 'POST'])
+@app.route("/API")
 def modePage():
-    mode = "mode_1"
-    if (flask.request.method=='POST'):
-        mode = flask.request.form["mode"]
-    else:
-        mode = flask.request.args.get("mode")
-    if (mode == "mode_1"):
-        requests.post('http://192.168.178.199/', data="0r\n")
-    elif (mode == "mode_2"):
-        requests.post('http://192.168.178.199/', data="1g\n")
-    else:
-        mydata = mode+"\n"
-        app.logger.info('my mydata is %s and mode is %s\n', mydata, mode)
-        requests.post('http://192.168.178.199/', data=mydata)
-    app.logger.info('the mode in gereral is %s\n', mode)
+    message = flask.request.args.get("mode")
+    app.logger.info("message: >%s<\n", message)
+    requests.post('http://192.168.178.199/', data=message + "\n")
     return ("" ,204)
 
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')    
+    app.run(host='0.0.0.0')
