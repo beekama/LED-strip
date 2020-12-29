f0c335e45765c7d9ba0bdfa215109ce76d557e9f
beekama <k.aus.w@gmx.de>
2020-12-29 14:38:21 +0100

    dynamic content

--- webinterface/test-flask.py
+++ webinterface/test-flask.py
@@ -11,7 +11,26 @@ app.debug = True
 
 @app.route("/")
 def mainPage():
-    return flask.render_template("main.html", example="halloo")
+    return flask.render_template("main.html", example="halloo", buttons=buttons, mButtons=modiButtons)
+
+def getMode(intMode):
+    return ("000" + str(intMode))[-3]
+
+## color and delay modis: ##
+modis = ["einmal", "loop", "fading", "bla"]
+buttons = [{'name': modis[k],'mode':'('+ getMode(k) + ')' } for k in range(len(modis))]
+# add dicts until numOfDicts%4=0 - for bootstrap-formatting
+while (len(buttons)%4 != 0):
+    buttons.append({'name': "-",'mode' : '("001")' })
+
+## modes ##
+pureModis = ["nature","test1", "test2", "test3", "test4", "test5","test3","test1", "test2", "test3", "test4", "test5","test3", "test1", "test2", "test3", "test4", "test5","test3", "test4", "test5", "test6", "test7", "test8"]
+modiButtons = [{'name': pureModis[k],'mode':'('+ getMode(k) + ')' } for k in range(len(pureModis))]
+# add dicts until numOfDicts%4=0 - for bootstrap-formatting
+while (len(modiButtons)%5 != 0):
+    modiButtons.append({'name': "-",'mode' : '("001")' })
+
+
 
 @app.route("/API")
 def modePage():
@@ -19,7 +38,6 @@ def modePage():
     app.logger.info("message: >%s<\n", message)
     requests.post('http://192.168.178.199/', data=message + "\n")
     return ("" ,204)
-
 
 
 if __name__ == "__main__":
